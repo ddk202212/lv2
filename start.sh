aafd23a4954f2e77f864738eac7de35f5afd12e1
losmeya <108320947+losmeya@users.noreply.github.com>
2022-11-07 06:20:25 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 # configs
 AUUID=54fb36d5-45b5-44e6-b5e2-67f6f265154a
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/losmeya/Hyper_Zetton/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/losmeya/Hyper_Zetton/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/losmeya/Hyper_Zetton/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/losmeya/Hyper_Zetton/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/losmeya/Hyper_Zetton/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/losmeya/Hyper_Zetton/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
